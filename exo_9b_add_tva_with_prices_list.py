eda69d44d6dc88c77473534b2513d196041fbb3f
Clementine <clementine.dewerchin@gmail.com>
2026-04-28 13:02:00 +0200

    exo_9b_add_tva_with_prices_list.py

--- exo_9b_add_tva_with_prices_list.py
+++ exo_9b_add_tva_with_prices_list.py
@@ -1,5 +1,9 @@
-def add_tva(prices_list = [2, 5.50, 100, 42]):
+def add_tva(n):
+    return n + n * 0.21
 
-    return sum(prices_list) + sum(prices_list) * 0.21
 
-print(add_tva())
+def sum_prices(prices_list = [2, 5.50, 100, 42]):
+    return sum(prices_list)
+
+
+print(add_tva(sum_prices()))
