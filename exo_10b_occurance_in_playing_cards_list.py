a89d4a12a5636538efcceff4857f61f75bbd1aff
Clementine <clementine.dewerchin@gmail.com>
2026-04-28 13:01:34 +0200

    exo_10b_occurance_in_playing_cards_list.py

--- exo_10b_occurance_in_playing_cards_list.py
+++ exo_10b_occurance_in_playing_cards_list.py
@@ -7,16 +7,17 @@ def random_card(n, VALUES = ["as", 2, 3, 4, 5, 6, 7, 8, 9, 10, "valet", "dame", 
         random_index = randint(0, len(VALUES) - 1)
         random_cards_list.append(VALUES[random_index])
     
-    counts = {x: random_cards_list.count(x) for x in set(random_cards_list)}
+    for value in set(random_cards_list):
+        counter = random_cards_list.count(value)
 
-    if 5 in counts.values():
-        print("Poker")
-    elif 4 in counts.values():
-        print("Carré")
-    elif 3 in counts.values():
-        print("Brelan")
-    elif 2 in counts.values():
-        print("Paire")
+        if counter == 5:
+            print("Poker")
+        elif counter == 4:
+            print("Carré")
+        elif counter == 3:
+            print("Brelan")
+        elif counter == 2:
+            print("Paire")
 
     return random_cards_list
 
