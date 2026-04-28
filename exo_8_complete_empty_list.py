3cff9a0a0ad47f315ad5a56c9d6a20e5cc54cb58
Clementine <clementine.dewerchin@gmail.com>
2026-04-28 13:02:26 +0200

    exo_8_complete_empty_list.py

--- exo_8_complete_empty_list.py
+++ exo_8_complete_empty_list.py
@@ -1,29 +1,14 @@
-def complete_list(letter, letters_list = ["a", "b", "c", ".", "e", ".", "g"]):
+def complete_list(letter, letters_list):
     i = -1
 
     for char in letters_list:
         if letters_list[i] == ".":
             letters_list[i] = letter
             i -= 1
+            print(letters_list)
         else:
             i -= 1
     return letters_list
 
-print(complete_list("X"))
-
-# EMPTY LIST
-
-def complete_list_2(letter, letters_list = [".", ".", ".", ".", ".", ".", "."]):
-    i = -1
-
-    for char in letters_list:
-            if letters_list[i] == ".":
-                letters_list[i] = letter
-                i -= 1 
-                print(letters_list)
-            else:
-                i -= 1        
-
-    return letters_list
-
-complete_list_2("X")
+complete_list("X", letters_list = ["a", "b", "c", ".", "e", ".", "g"])
+complete_list("X", letters_list = [".", ".", ".", ".", ".", ".", "."])
